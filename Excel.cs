04b49e98097bb72a4cc19c91156a065613f41667
Kristijan Čulina <kculina@foi.hr>
2021-10-26 22:40:25 +0200

    Add project files.

---
+++ Excel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+
+namespace read_write_excel
+{
+    class Excel
+    {
+        string path = "";
+        _Application excel = new _Excel.Application();
+        Workbook wb;
+        Worksheet ws;
+
+        public Excel(string path, int Sheet)
+        {
+            this.path = path;
+            wb = excel.Workbooks.Open(path);
+            ws = wb.Worksheets[Sheet];
+        }
+
+        public string ReadCell(int i, int j)
+        {
+            if (ws.Cells[i, j].Value2 != null )
+            {
+                return ws.Cells[i, j].Value2;
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using NPOI.SS.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System.IO;
+
+namespace read_write_excel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Excel excel = new Excel("", 1); //Insert path to the excel file
+
+            IWorkbook workbook = new XSSFWorkbook();
+            ISheet sheet1 = workbook.CreateSheet("Sheet1");
+
+            string[,] polje = new string[10000, 7];
+
+            IRow pomocniRow;
+            int brojacRedova = 1;
+            for (int i = 1; i < 581; i++)
+            {
+                
+                string id = excel.ReadCell(i, 1);
+                string type = excel.ReadCell(i, 2);
+                string title = excel.ReadCell(i, 3);
+                string authors = excel.ReadCell(i, 4);
+                string aff = excel.ReadCell(i, 5);
+                string emails = excel.ReadCell(i, 6);
+
+                string[] poljeAutora = authors.Split('|');
+                string[] poljeAff = aff.Split('|');
+                string[] poljeEmails = emails.Split('|');
+
+
+                for (int z = 0; z<poljeAutora.Count(); z++)
+                {
+                    pomocniRow = sheet1.CreateRow(brojacRedova);
+                    Console.WriteLine(poljeAutora.Count());
+                    pomocniRow.CreateCell(1).SetCellValue(id);
+                    pomocniRow.CreateCell(2).SetCellValue(type);
+                    pomocniRow.CreateCell(3).SetCellValue(title);
+                    pomocniRow.CreateCell(4).SetCellValue(poljeAutora[z]);
+                    pomocniRow.CreateCell(5).SetCellValue(poljeAff[z]);
+                    pomocniRow.CreateCell(6).SetCellValue(poljeEmails[z]);
+                    brojacRedova++;
+                }
+            }
+
+            FileStream sw = File.Create(""); //Insert name of the excel file that will be created - dont forget .xlsx
+            workbook.Write(sw);
+            sw.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
